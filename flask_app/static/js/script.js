f31b274902bfcb82a8b661f5181a4f917ecf6809
Dalius Beck <daliusbeckxiv@gmail.com>
2023-05-27 12:48:55 -0700

    update

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -24,20 +24,4 @@ async function github_api_call(element){
     `
 }
 
-// async function github_api_call() {
-//     let response = await fetch('https://api.github.com/users/DaliusBeckjr')
-//     let data = await response.json()
-//     console.log(data) // all of the user data in the console
-//     console.log(data.name)
-//     console.log(data.avatar_url)
-
-//     gitCard.innerHTML = `
-//         <br />
-//         <img src="${data.avatar_url}" alt="picture of ${data.name}" 
-//         class="w-50 p-3 rounded-circle">
-//         <h2> Hello ${data.name}!</h2>
-
-
-//     `
-// }
 
