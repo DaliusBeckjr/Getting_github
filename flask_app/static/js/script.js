2b761b489554dbe05c50605a5efb11cf12df7819
Dalius Beck <daliusbeckxiv@gmail.com>
2023-05-27 11:49:18 -0700

    updated getting github
    changed the async function into an onclick for user information to pop up successfully

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -5,13 +5,12 @@
 // attach a github user name to the end 
 // ex. /users/username (adion81)
 
-
-async function github_api_call() {
+async function github_api_call(element){
     let response = await fetch('https://api.github.com/users/DaliusBeckjr')
     let data = await response.json()
-    console.log(data) // all of the user data in the console
-    console.log(data.name)
-    console.log(data.avatar_url)
+    console.log(data) //  user data 
+    console.log(data.name) // user name
+    console.log(data.avatar_url) // user profile picture
 
     gitCard.innerHTML = `
         <br />
@@ -23,4 +22,20 @@ async function github_api_call() {
     `
 }
 
-github_api_call()
+// async function github_api_call() {
+//     let response = await fetch('https://api.github.com/users/DaliusBeckjr')
+//     let data = await response.json()
+//     console.log(data) // all of the user data in the console
+//     console.log(data.name)
+//     console.log(data.avatar_url)
+
+//     gitCard.innerHTML = `
+//         <br />
+//         <img src="${data.avatar_url}" alt="picture of ${data.name}" 
+//         class="w-50 p-3 rounded-circle">
+//         <h2> Hello ${data.name}!</h2>
+
+
+//     `
+// }
+
